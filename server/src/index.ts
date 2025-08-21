6da70b45d5935680cc7cc9735a52881c984cae6f
DerianPaez <derianpaez01@hotmail.com>
2025-08-21 16:00:18 -0500

    setup supabase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import supabase from "../config/supabase-client";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await supabase.from("users").select();
+  res.json(users);
 });
 
 const PORT = process.env.PORT || 3000;
